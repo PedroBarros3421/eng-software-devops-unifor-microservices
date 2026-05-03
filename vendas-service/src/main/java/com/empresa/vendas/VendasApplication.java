b99ba578b7127037544924b5fd3919e3f873ff55
Victor Kauan <victorkauan.dev@gmail.com>
2026-05-02 23:33:20 -0300

    chore: Configura Swagger para apontar pro API gateway

--- vendas-service/src/main/java/com/empresa/vendas/VendasApplication.java
+++ vendas-service/src/main/java/com/empresa/vendas/VendasApplication.java
@@ -1,9 +1,12 @@
 package com.empresa.vendas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@OpenAPIDefinition(servers = { @Server(url = "/") })
 @SpringBootApplication
 @EnableFeignClients
 public class VendasApplication {
